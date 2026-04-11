6d2bdf25ff0880b7f6f83b74c539878e273c79b1
nuralidoschanov <doschanovnurali69@gmail.com>
2026-04-11 15:24:33 +0500

    KEP Algoritms

---
+++ 21.py
@@ -0,0 +1,4 @@
+n = int(input())
+s = n * (n + 1) * (n + 2)
+q = s % 6
+print(q)
---
+++ 4.py
@@ -0,0 +1,12 @@
+import math
+a = int(input())
+b = int(input())
+c = (a+b)/2
+print(c)
+d = math.sqrt(a*b)
+f = (2*a*b)/(a+5)
+print(c)
+print(d)
+print(f)
+
+
