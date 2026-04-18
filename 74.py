2698507214826857d7c877d7ee16d0d3ebe9cd41
nuralidoschanov <doschanovnurali69@gmail.com>
2026-04-18 16:33:56 +0500

    KEP Algaritim

---
+++ 74.py
@@ -0,0 +1,8 @@
+def even_index_or_value(list):
+    new_list = []
+    for index in range(len(list)):
+        if index % 2 == 0 or list(index) % 2 == 0 :
+            new_list.append(list[index])
+
+    return new_list
+
